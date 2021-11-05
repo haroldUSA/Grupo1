c34f93e77dbde8fba9435d8022ecb580c45bb449
DESKTOP-81RP8J0\harld <hars1302@gmail.com>
2021-11-05 17:39:06 -0500

    adicion de login y register en el index

--- costumeweb/src/main/java/com/costume/CostumeApplication.java
+++ costumeweb/src/main/java/com/costume/CostumeApplication.java
@@ -1,13 +1,61 @@
 package com.costume;
 
+import java.util.Collections;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+//@RestController
 @SpringBootApplication
-public class CostumeApplication {
+public class CostumeApplication extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CostumeApplication.class, args);
-	}
+//    @GetMapping("/user")
+//    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+//        return Collections.singletonMap("name", principal.getAttribute("name"));
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostumeApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                .antMatchers("/", "/error", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login()
+                .and()
+                .logout(l -> l
+                .logoutSuccessUrl("/").permitAll()
+                );
+    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        // @formatter:off
+//        http
+//            .authorizeRequests()
+//                .antMatchers("/", "/home").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//            .formLogin()
+//                .loginPage("/login.html")
+//                .permitAll()
+//                .and()
+//            .logout()
+//                .permitAll();
+//    }
 
 }
